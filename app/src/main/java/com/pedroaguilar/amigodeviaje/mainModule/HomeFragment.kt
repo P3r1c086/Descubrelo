d68788f2c76db65db22132590932a5d8c9898e21
pajar <pajaros33@gmail.com>
2023-01-23 20:55:33 +0100

    Carrusel imagenes en HomeFragment.kt

--- app/src/main/java/com/pedroaguilar/amigodeviaje/mainModule/HomeFragment.kt
+++ app/src/main/java/com/pedroaguilar/amigodeviaje/mainModule/HomeFragment.kt
@@ -5,14 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.pedroaguilar.amigodeviaje.R
 import com.pedroaguilar.amigodeviaje.databinding.FragmentHomeBinding
-import com.pedroaguilar.amigodeviaje.common.entities.utils.HomeAux
+import com.pedroaguilar.amigodeviaje.common.utils.HomeAux
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 
 class HomeFragment : Fragment(), HomeAux {
 
     private lateinit var binding: FragmentHomeBinding
-
+    private val list = mutableListOf<CarouselItem>()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
     Bundle?
     ): View? {
@@ -22,10 +25,19 @@ class HomeFragment : Fragment(), HomeAux {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        loadCarruselImagesList()
     }
 
     override fun goToTop() {
 //        binding.recyclerView.smoothScrollToPosition(0)
+    }
+
+    private fun loadCarruselImagesList(){
+        val carousel: ImageCarousel = binding.carousel
+        list.add(CarouselItem(imageDrawable = R.drawable.alhambra, caption = "Alhambra") )
+        list.add(CarouselItem(imageDrawable = R.drawable.playa_granada, caption = "Playa Granada"))
+        list.add(CarouselItem(imageDrawable = R.drawable.sierra_nevada, caption = "Sierra Nevada"))
+        //carousel.autoPlay = true
+        carousel.addData(list)
     }
 }
