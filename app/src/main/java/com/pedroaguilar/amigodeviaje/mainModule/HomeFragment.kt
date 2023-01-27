d03b660af50c7b26281a39bdf41bad3ce9345b30
P3r1c086 <80258593+P3r1c086@users.noreply.github.com>
2023-01-26 20:57:49 -0800

    Arreglado carrousel.

--- app/src/main/java/com/pedroaguilar/amigodeviaje/mainModule/HomeFragment.kt
+++ app/src/main/java/com/pedroaguilar/amigodeviaje/mainModule/HomeFragment.kt
@@ -1,24 +1,29 @@
 package com.pedroaguilar.amigodeviaje.mainModule
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import com.pedroaguilar.amigodeviaje.MainActivity
 import com.pedroaguilar.amigodeviaje.R
 import com.pedroaguilar.amigodeviaje.databinding.FragmentHomeBinding
-import com.pedroaguilar.amigodeviaje.common.utils.HomeAux
-import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+import org.imaginativeworld.whynotimagecarousel.listener.CarouselListener
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
-    private val list = mutableListOf<CarouselItem>()
+    private val list : List<CarouselItem> = listOf(
+        CarouselItem(imageDrawable = R.drawable.alhambra, caption = "Alhambra"),
+        CarouselItem(imageDrawable = R.drawable.playa_granada, caption = "Playa Granada"),
+        CarouselItem(imageDrawable = R.drawable.sierra_nevada, caption = "Sierra Nevada")
+    )
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
     Bundle?
-    ): View? {
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
@@ -28,16 +33,17 @@ class HomeFragment : Fragment() {
         loadCarruselImagesList()
     }
 
-//    override fun goToTop() {
-////        binding.recyclerView.smoothScrollToPosition(0)
-//    }
-
     private fun loadCarruselImagesList(){
-        val carousel: ImageCarousel = binding.carousel
-        list.add(CarouselItem(imageDrawable = R.drawable.alhambra, caption = "Alhambra") )
-        list.add(CarouselItem(imageDrawable = R.drawable.playa_granada, caption = "Playa Granada"))
-        list.add(CarouselItem(imageDrawable = R.drawable.sierra_nevada, caption = "Sierra Nevada"))
-        //carousel.autoPlay = true
-        carousel.addData(list)
+        with (binding.carousel){
+            autoPlay = true
+            addData(list)
+            carouselListener = object : CarouselListener {
+                override fun onClick(position: Int, carouselItem: CarouselItem) {
+                    //Todo: enviar argumentos con informacion del item pulsado
+                    ((activity as MainActivity).supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment)
+                        .navController.navigate(R.id.action_homeFragment_to_detailsFragment)
+                }
+            }
+        }
     }
 }
